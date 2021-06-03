87984ab9ed1e4ef6178ca808b5d8e1f5190527c1
Danielauler <daniel-auler@hotmail.com>
2021-06-03 16:47:19 -0300

    fixed percentage

--- src/components/KeyboardPanel/KeyboardPanel.tsx
+++ src/components/KeyboardPanel/KeyboardPanel.tsx
@@ -6,19 +6,52 @@ import Display from '@components/Display/Display'
 const KeyboardPanel: React.FC = () => {
   const [displayValue, setDisplayValue] = useState<string>('0')
 
-  function handleClick(event: React.FormEvent<HTMLInputElement>): void {
-    const buttonName: string = event.currentTarget.innerText
-    switch (buttonName) {
-      case 'AC':
-        setDisplayValue('0')
-        break
-      case '=':
-        setDisplayValue(evaluate(displayValue.replace('x', '*').replace('÷', '/')).toString())
-        break
-      default:
-        if (displayValue === '0') setDisplayValue(buttonName)
-        else setDisplayValue((value) => `${value}${buttonName}`)
-        break
+  const verifyPercentage = (value: string): string => {
+    let input = value
+    const pcntReg = /[0-9]*\.?[0-9]%/g
+    const numReg = /[0-9]*\.?[0-9]/g
+
+    while (input.match(pcntReg)) {
+      let leftVal = '1'
+      const right = input.match(pcntReg) ?? ''
+      const left = input.split(right[0])
+      if (left?.length > 0) {
+        const leftTrim = left[0].trim()
+        leftVal = leftTrim.slice(0, -1)
+        leftVal = evaluate(leftVal).toString()
+      }
+
+      const pcntVal = right[0]?.match(numReg) ?? '1'
+
+      const newval = evaluate(`${leftVal}*${pcntVal[0]}/100`).toString()
+      input = input.replace(left[0] + right[0], evaluate(left[0] + newval).toString())
+    }
+
+    return evaluate(input).toString()
+  }
+
+  const handleClick = (event: React.FormEvent<HTMLInputElement>): void => {
+    try {
+      const buttonName: string = event.currentTarget.innerText
+      switch (buttonName) {
+        case 'AC':
+          setDisplayValue('0')
+          break
+        case '=':
+          setDisplayValue(verifyPercentage(displayValue.replace('x', '*').replace('÷', '/')))
+          break
+        case '+/-':
+          setDisplayValue(
+            (parseInt(verifyPercentage(displayValue.replace('x', '*').replace('÷', '/')), 10) * -1).toString()
+          )
+          break
+        default:
+          if (displayValue === '0') setDisplayValue(buttonName)
+          else setDisplayValue((value) => `${value}${buttonName}`)
+          break
+      }
+    } catch {
+      setDisplayValue(displayValue ?? '0')
     }
   }
 
